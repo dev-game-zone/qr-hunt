0df174d4da34446a9ea6fa60f292c1c2fccf28b6
Gary Dutton <23572424+peakchallenge@users.noreply.github.com>
2025-11-13 18:39:19 +0000

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -3,7 +3,15 @@ const CONFIG = {
   SECRET_SALT: "YOUR_SECRET_SALT",
   CODE_LENGTH: 8,
   LETTER_MAP: {
-    "QR001": "A"
+    "QR001": "D",
+    "QR002": "A",
+    "QR003": "N",
+    "QR001": "D",
+    "QR004": "E",
+    "QR005": "L",
+    "QR006": "I",
+    "QR007": "O",
+    "QR008": "N"
   },
-  HIDDEN_PHRASE: "A"
+  HIDDEN_PHRASE: "DANDELION"
 };
