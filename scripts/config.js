372e29d2c5ce30c361777b0e3732c855f23e7111
Gary Dutton <Gary.Dutton@unilever.com>
2025-11-18 09:39:57 +0000

    New Word

--- scripts/config.js
+++ scripts/config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-   BASE_PATH: window.location.hostname === "127.0.0.1" || 
-             window.location.hostname === "localhost"
-             ? "/"
-             : "/qr-hunt/",   // your GitHub Pages repo folder
+  BASE_PATH: window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "localhost"
+    ? "/"
+    : "/qr-hunt/",   // your GitHub Pages repo folder
   SECRET_SALT: "YOUR_SECRET_SALT",
   CODE_LENGTH: 8,
   LETTER_MAP: {
-    "QR001": "D",
-    "QR002": "A",
-    "QR003": "N",
-    "QR001": "D",
-    "QR004": "E",
-    "QR005": "L",
-    "QR006": "I",
-    "QR007": "O",
-    "QR003": "N",
+    "QR001": "U",
+    "QR002": "N",
+    "QR003": "I",
+    "QR001": "Q",
+    "QR004": "U",
+    "QR005": "E",
+    "QR006": "*",
+    "QR007": "*",
+    "QR003": "*",
     "QR008": "*",
     "QR009": "*",
     "QR010": "*"
